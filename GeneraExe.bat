8249b8cdfdbb8d160684dfb1ce6ea254c584bfe1
leyluchy <valeria.ayelen@gmail.com>
2021-07-24 17:19:01 -0300

    nuevos tests

--- GeneraExe.bat
+++ GeneraExe.bat
@@ -13,13 +13,11 @@ TPFinal.exe "./tests/3 if true.txt" "Polaca-if true.txt"
 TPFinal.exe "./tests/4 if false.txt" "Polaca-if false.txt"
 TPFinal.exe "./tests/5 while.txt" "Polaca-while.txt"
 TPFinal.exe "./tests/6 arithmetics.txt" "Polaca-arithmetics.txt"
-TPFinal.exe "./tests/6 arithmetics1.txt" "Polaca-arithmetics1.txt"
-TPFinal.exe "./tests/6 arithmetics2.txt" "Polaca-arithmetics2.txt"
-TPFinal.exe "./tests/6 arithmetics3.txt" "Polaca-arithmetics3.txt"
-TPFinal.exe "./tests/6 arithmetics4.txt" "Polaca-arithmetics4.txt"
+TPFinal.exe "./tests/6 arithmetics div float.txt" "Polaca-arithmetics-div-float.txt"
+TPFinal.exe "./tests/6 arithmetics div int.txt" "Polaca-arithmetics-div-int.txt"
+TPFinal.exe "./tests/6 arithmetics mod.txt" "Polaca-arithmetics-mod.txt"
 TPFinal.exe "./tests/7 conditions.txt" "Polaca-conditions.txt"
 TPFinal.exe "./tests/8 asignacion multiple.txt" "Polaca-asignacion multiple.txt"
-TPFinal.exe "./tests/9 suma.txt" "Polaca-suma.txt"
 pause
 del lex.yy.c
 del y.tab.c
